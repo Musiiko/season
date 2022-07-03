32ab4b3046402a87b0a522c72cc5ed20c28b7e80
MSP1981 <99791179+MSP1981@users.noreply.github.com>
2022-07-03 14:43:59 +0300

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,19 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner num = new Scanner(System.in);
+        System.out.print("Enter the serial number of the month = ");
+        int i = num.nextInt();
+
+        System.out.println(
+                switch (i){
+                    case 12,1,2 -> "Season: winter";
+                    case 3,4,5 -> "Season: spring";
+                    case 6,7,8 -> "Season: summer";
+                    case 9,10,11 -> "Season: autumn";
+                    default -> "Ouch ! Not the ordinal number of the month! Repeat one more time";
+                }
+        );
     }
 }
